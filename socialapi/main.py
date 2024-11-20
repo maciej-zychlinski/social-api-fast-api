054595f5c453bcefcb87faaf5097dbafad6fc3f9
Maciej <mcj.zychlinski@gmail.com>
2024-11-20 18:56:03 +0100

    add APIrouter

--- socialapi/main.py
+++ socialapi/main.py
@@ -1,22 +1,8 @@
 from fastapi import FastAPI
 
-from socialapi.models.post import UserPost, UserPostIn
+from socialapi.routers.post import router as post_router
 
 app = FastAPI()
 
 
-post_table = {}
-
-
-@app.post("/post", response_model=UserPost)
-async def creat_post(post: UserPostIn):
-    data = post.model_dump()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+app.include_router(post_router)
