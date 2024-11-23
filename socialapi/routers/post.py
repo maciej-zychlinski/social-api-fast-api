703725ddf5c83f191acb9daffceb3f76c21da57c
Maciej <mcj.zychlinski@gmail.com>
2024-11-23 15:36:37 +0100

    add endpoint to create comments

--- socialapi/routers/post.py
+++ socialapi/routers/post.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from socialapi.models.post import UserPost, UserPostIn
+from socialapi.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
 
 router = APIRouter()
 
 
 post_table = {}
+comment_table = {}
+
+
+def find_post(post_id: int):
+    return post_table.get(post_id)
 
 
 @router.post("/post", response_model=UserPost)
@@ -17,6 +28,37 @@ async def creat_post(post: UserPostIn):
     return new_post
 
 
-@router.get("/post", response_model=list[UserPost])
+@router.get("/post", response_model=list[UserPost], status_code=201)
 async def get_all_posts():
     return list(post_table.values())
+
+
+@router.post("/comment", response_model=Comment, status_code=201)
+async def creat_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
+@router.get("/post/{post_id}/comment", response_model=list[Comment])
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_posts_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {
+        "post": post,
+        "comments": await get_comments_on_post(post_id),
+    }
